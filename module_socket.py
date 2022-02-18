da1f3ca58012e00eeb2be1f7bcf030a11f5a406d
Ali Sefer <32126145+AliSfr@users.noreply.github.com>
2022-02-18 14:55:31 +0300

    Add files via upload

---
+++ module_socket.py
@@ -0,0 +1,26 @@
+import socket
+port_list = []
+banner_list = []
+with open("ip.txt","r",encoding="utf-8") as file:
+    icerik = file.read()
+for ip in icerik.splitlines():
+    print(ip)
+    for port in range(1,25):
+        try:
+            soket=socket.socket()
+            soket.connect((str(ip),int(port)))
+            banner=soket.recv(1024)
+            banner_list.append(str(banner))
+            port_list.append(str(port))
+            soket.close()
+            print(port)
+            print(banner)
+            if "SSH" in str(banner):
+                print("System can be Linux or Network device!") 
+                log = str(ip)+"\n"
+                with open("linux.txt","a",encoding="utf-8") as file:
+                    file.write(log)
+        except:
+            pass
+print(port_list)
+print(banner_list)
